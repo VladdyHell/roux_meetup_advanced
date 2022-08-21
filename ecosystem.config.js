a93e284a41664081984ad83f547e11ba7f0fd917
Vladd Cantor <crimsonvirtuoso@vladdyhell.bbrouter>
2022-08-21 19:00:25 +0800

    Update PM2 config to local

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,15 +11,19 @@ module.exports = {
   ],
   deploy: {
     production: {
-      user: 'nodejs',
-      host: '<CHANGE_TO_YOUR_HOST>',
+      user: 'crimsonvirtuoso',
+      // host: '<CHANGE_TO_YOUR_HOST>',
+      host: '192.168.1.3',
       ref: 'origin/master',
-      repo: '<CHANGE_TO_YOUR_GITHUB_REPO>',
+      // repo: '<CHANGE_TO_YOUR_GITHUB_REPO>',
+      repo: 'https://github.com/vladdyhell/roux_meetup_advanced',
 
       // Make sure this directory exists on your server or change this entry to match your directory structure
-      path: '/home/nodejs/deploy',
+      // path: '/home/nodejs/deploy',
+      path: '/home/crimsonvirtuoso/deploy',
 
-      'post-deploy': 'cp ../.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production',
+      // 'post-deploy': 'cp ../.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy': 'pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
